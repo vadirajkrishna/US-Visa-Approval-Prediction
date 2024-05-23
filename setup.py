d8e6fda8ebeb9e0b01b9f9bad11ca42d8c31947c
Vadi <vadirajkrishna@gmail.com>
2024-05-23 20:33:34 +0530

    corrected the config file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Vadiraj"
+    author_email="vadirajkrishna@gmail.com",
+    packages=find_packages()
+)
--- template.py
+++ template.py
@@ -12,7 +12,7 @@ list_of_files = [
     f"{project_name}/components/model_trainer.py",
     f"{project_name}/components/model_evaluation.py",
     f"{project_name}/components/model_pusher.py",
-    f"{project_name}/configuration/__init__.py"
+    f"{project_name}/configuration/__init__.py",
     f"{project_name}/configuration/aws_connection.py",
     f"{project_name}/constants/__init__.py",
     f"{project_name}/entity/__init__.py",
